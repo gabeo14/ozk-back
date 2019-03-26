b3acbb4c36fceec3c4e048da7cef4df50876649e
Gabe Oleinik <gomusic14@gmail.com>
2019-03-26 19:39:30 -0400

    created backend and scaffolded

---
+++ SavingsGoalDatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ozk_back
+{
+    public partial class SavingsGoalDatabaseContext : DbContext
+    {
+        public SavingsGoalDatabaseContext()
+        {
+        }
+
+        public SavingsGoalDatabaseContext(DbContextOptions<SavingsGoalDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("server=localhost;database=SavingsGoalDatabase");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.3-servicing-35854");
+        }
+    }
+}
